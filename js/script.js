eaa3a2d3c0644ccdeb4355ecbb54ad0fda056302
alouisa <annaastle5@gmail.com>
2020-04-25 11:47:47 -0700

    commit

--- js/script.js
+++ js/script.js
@@ -1,2 +1,4 @@
-let dateModified = new Date(document.lastModified);
-document.getElementById("todaysDate").innerHTML = "Last Updated: " + dateModified;
+function lastModified() {
+        let date = new Date(document.lastModified);
+        document.getElementById("todaysDate").innerHTML = `Last Updated: ${date.getMonth} / ${date.getDay} / ${date.getFullYear}${date.getHours}:${date.getUTCMinutes}:${date.getUTCMilliseconds}`;
+    }
