6fb50ce005a9d1d6135bfa6718b076953a2468c4
alouisa <annaastle5@gmail.com>
2020-04-25 12:20:05 -0700

    commit

--- js/script.js
+++ js/script.js
@@ -1,7 +1,2 @@
-        let date = new Date(document.lastModified);
-        let month = date.getMonth();
-        let day = date.getDay();
-        let year = date.getFullYear();
-        let time = date.getTime(); 
-        let message = `Last Updated: ${month} / ${day} / ${year} ${time}`;
-        document.getElementById("dateOutput").innerHTML = message;
+let lastmod = document.lastModified;
+document.getElementById("dateOutput").innerHTML = lastmod;      
