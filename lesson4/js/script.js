99bcb759a49320b7bf2ee23d18cd3bdeebc1a0b2
alouisa <annaastle5@gmail.com>
2020-05-12 09:37:22 -0700

    commit

--- lesson4/js/script.js
+++ lesson4/js/script.js
@@ -1,5 +1,3 @@
 let date = new Date;
-let year = date.getFullYear();
-document.getElementById("year").innerHTML = year;
 let lastmod = document.lastModified;
 document.getElementById("dateOutput").innerHTML = lastmod;      
