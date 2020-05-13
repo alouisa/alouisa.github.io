62b2b32d2b18755733e51331b70e013b8ddb19ac
alouisa <annaastle5@gmail.com>
2020-05-12 21:34:45 -0700

    commit

--- lesson4/js/script.js
+++ lesson4/js/script.js
@@ -1,3 +1,17 @@
+const monthsOfYear = ["January", "February", "March", "April", "May",
+    "June", "July", "August", "September", "October", "November", "December"];
+const daysOfTheWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", 
+    "Friday", "Saturday"];
+let date = new Date();
+let m = date.getMonth();
+let month = monthsOfYear[m]; 
+let d = date.getDay();
+let dayOfWeek = daysOfTheWeek[d]; 
+let day = date.getDate();
+let year = date.getFullYear();
+let todaysDate = `${dayOfWeek}, ${day} ${month} ${year}`;
+document.getElementById("todaysDate").innerHTML = todaysDate; 
+
 function toggleMenu(){
     document.getElementById("primaryNav").classList.toggle("hide"); 
 }
