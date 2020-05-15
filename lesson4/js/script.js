b31c5103629a7c6b052bb0e4083707ae366c4a06
alouisa <annaastle5@gmail.com>
2020-05-15 12:21:07 -0700

    commit

--- lesson4/js/script.js
+++ lesson4/js/script.js
@@ -3,13 +3,11 @@ const monthsOfYear = ["January", "February", "March", "April", "May",
 const daysOfTheWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", 
     "Friday", "Saturday"];
 let date = new Date();
-let m = date.getMonth();
-let month = monthsOfYear[m]; 
+let m = date.getMonth(); 
 let d = date.getDay();
-let dayOfWeek = daysOfTheWeek[d]; 
 let day = date.getDate();
 let year = date.getFullYear();
-let todaysDate = `${dayOfWeek}, ${day} ${month} ${year}`;
+let todaysDate = `${daysOfTheWeek[d]}, ${day} ${monthsOfYear[m]} ${year}`;
 document.getElementById("todaysDate").innerHTML = todaysDate; 
 
 function toggleMenu(){
