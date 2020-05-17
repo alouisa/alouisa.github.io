2372c483a69e0fefe36200ac9a7e3f25293eff90
alouisa <annaastle5@gmail.com>
2020-05-16 17:09:23 -0700

    commit

--- lesson5/js/script.js
+++ lesson5/js/script.js
@@ -2,13 +2,21 @@ const monthsOfYear = ["January", "February", "March", "April", "May",
     "June", "July", "August", "September", "October", "November", "December"];
 const daysOfTheWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", 
     "Friday", "Saturday"];
+const announcement = document.querySelector(".announcement");
 let date = new Date();
 let m = date.getMonth(); 
 let d = date.getDay();
 let day = date.getDate();
 let year = date.getFullYear();
 let todaysDate = `${daysOfTheWeek[d]}, ${day} ${monthsOfYear[m]} ${year}`;
-document.getElementById("todaysDate").innerHTML = todaysDate; 
+document.getElementById("todaysDate").innerHTML = todaysDate;
+
+if (d == 5){
+    announcement.setAttribute("style", "display: block");
+}
+else {
+    // announcement.setAttribute("style", "display: none"); 
+}
 
 function toggleMenu(){
     document.getElementById("primaryNav").classList.toggle("hide"); 
