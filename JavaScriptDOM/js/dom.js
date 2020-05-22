a7e3dd3d92cc5e4c25a422f3c077a2a4e00cacfb
alouisa <annaastle5@gmail.com>
2020-05-22 16:16:30 -0700

    commit

---
+++ JavaScriptDOM/js/dom.js
@@ -0,0 +1,5 @@
+let paragraphs = document.querySelectorAll("p");
+// paragraphs.forEach(function(p){p.innerHTML = "This is new"});
+paragraphs.forEach((p)=> p.innerHTML = "This is new");
+paragraphs.forEach((p)=> p.style.color = "red");
+
