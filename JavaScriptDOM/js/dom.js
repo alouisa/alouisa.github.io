3c12cff30ed830b046a22d1da7747588704109be
alouisa <annaastle5@gmail.com>
2020-05-22 17:20:18 -0700

    commit

--- JavaScriptDOM/js/dom.js
+++ JavaScriptDOM/js/dom.js
@@ -1,5 +1,17 @@
 
 
+
+
+
+
+
+//can't get this code to work:
+// let test = document.getElementsByClassName("list");
+// test.addEventListener("mouseover", function (event) {
+//     // highlight the mouseover target
+//     event.target.style.color = "purple";})
+
+
 // let paragraphs = document.querySelectorAll("p");
 // // paragraphs.forEach(function(p){p.innerHTML = "This is new"});
 // paragraphs.forEach((p)=> p.innerHTML = "This is new");
