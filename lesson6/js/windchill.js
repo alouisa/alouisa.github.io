f2e12871a69472e7a7cd6d964df3ed14094931c9
alouisa <annaastle5@gmail.com>
2020-05-26 13:36:40 -0700

    commit

--- lesson6/js/windchill.js
+++ lesson6/js/windchill.js
@@ -2,5 +2,13 @@ let tempF = document.querySelector("#highTemp").innerHTML;
 let windSpeed = document.querySelector("#windSpeed").innerHTML;
 let t = parseFloat(tempF);
 let s = parseFloat(windSpeed);
-let windchill = (35,74 + (0.6215 * t) - (35.75 * Math.pow(s, 0.16)) + (0.4275* t * Math.pow(s, 0.16)));
-document.querySelector("#windchill").innerHTML = Math.round(windchill);
+let windchill = (35.74 + (0.6215 * t) - (35.75 * Math.pow(s, 0.16)) + (0.4275* t * Math.pow(s, 0.16)));
+if(tempF <= 50 && windSpeed >= 3.0){
+    document.querySelector("#windchill").innerHTML = Math.round(windchill);
+}
+else{
+    document.querySelector("#windchill-container").innerHTML = "Wind Chill: N/A"; 
+}
+
+
+
