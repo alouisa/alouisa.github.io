6acf0ddecb36363072a600e4ed8bc46eafe7596a
alouisa <annaastle5@gmail.com>
2020-05-26 13:16:39 -0700

    commit

--- lesson6/js/windchill.js
+++ lesson6/js/windchill.js
@@ -3,4 +3,4 @@ let windSpeed = document.querySelector("#windSpeed").innerHTML;
 let t = parseFloat(tempF);
 let s = parseFloat(windSpeed);
 let windchill = (35,74 + (0.6215 * t) - (35.75 * Math.pow(s, 0.16)) + (0.4275* t * Math.pow(s, 0.16)));
-document.querySelector("#windchill").innerHTML = windchill;
+document.querySelector("#windchill").innerHTML = Math.round(windchill);
