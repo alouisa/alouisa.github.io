80bea2a4160b295aefa98a27d4454ac05d63ea60
alouisa <annaastle5@gmail.com>
2020-05-26 13:12:34 -0700

    commit

--- lesson6/js/windchill.js
+++ lesson6/js/windchill.js
@@ -1,5 +1,5 @@
-let tempF = document.querySelector("#highTemp").value;
-let windSpeed = document.querySelector("#windSpeed").value;
+let tempF = document.querySelector("#highTemp").innerHTML;
+let windSpeed = document.querySelector("#windSpeed").innerHTML;
 let t = parseFloat(tempF);
 let s = parseFloat(windSpeed);
 let windchill = (35,74 + (0.6215 * t) - (35.75 * Math.pow(s, 0.16)) + (0.4275* t * Math.pow(s, 0.16)));
