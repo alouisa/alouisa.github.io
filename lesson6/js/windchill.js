4edecac3039c295856cd2928c68f14b965e214af
alouisa <annaastle5@gmail.com>
2020-05-28 18:54:41 -0700

    commit

--- lesson6/js/windchill.js
+++ lesson6/js/windchill.js
@@ -1,9 +1,7 @@
-let tempF = document.querySelector("#highTemp").innerHTML;
-let windSpeed = document.querySelector("#windSpeed").innerHTML;
-let t = parseFloat(tempF);
-let s = parseFloat(windSpeed);
-let windchill = (35.74 + (0.6215 * t) - (35.75 * Math.pow(s, 0.16)) + (0.4275* t * Math.pow(s, 0.16)));
-if(tempF <= 50 && windSpeed >= 3.0){
+let t = parseFloat(document.querySelector("#highTemp").innerHTML);
+let s = parseFloat(document.querySelector("#windSpeed").innerHTML);
+let windchill = 35.74 + 0.6215 * t - 35.75 * Math.pow(s, 0.16) + 0.4275* t * Math.pow(s, 0.16);
+if(t <= 50 && s >= 3.0){
     document.querySelector("#windchill").innerHTML = Math.round(windchill);
 }
 else{
