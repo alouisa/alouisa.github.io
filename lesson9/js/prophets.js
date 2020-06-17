e03ebe5de692f5233f541469e90532ab4635993f
alouisa <annaastle5@gmail.com>
2020-06-16 21:41:21 -0700

    commit

--- lesson9/js/prophets.js
+++ lesson9/js/prophets.js
@@ -6,3 +6,4 @@ fetch(requestURL)
 .then(function (jsonObject) {
     console.table(jsonObject);  // temporary checking for valid response and data parsing
 });
+const prophets = jsonObject['prophets'];
