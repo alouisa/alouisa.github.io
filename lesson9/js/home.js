b5434ba6994f8dd7ecdf160b8c63ee7b5cf4b605
alouisa <annaastle5@gmail.com>
2020-06-18 10:50:12 -0700

    commit

--- lesson9/js/home.js
+++ lesson9/js/home.js
@@ -6,17 +6,20 @@ fetch(requestURL)
     .then(function (jsonObject) {
         // console.table(jsonObject);  // temporary checking for valid response and data parsing
         const towns = jsonObject['towns'];
-        prophets.forEach(prophet => {
+        towns.forEach(town => {
             let card = document.createElement('section');
             let h2 = document.createElement('h2');
+            let h3 = document.createElement('h3');
             let p1 = document.createElement('p');
             let p2 = document.createElement('p');
+            let p3 = document.createElement('p');
             let image = document.createElement('img');
-            let fullName = prophet.name + ' ' + prophet.lastname;
 
-            h2.textContent = fullName;
+            h2.textContent = town.name;
+            h3.innerHTML = ;
             p1.textContent = "Date of Birth: " + prophet.birthdate;
-            p2.textContent = "Place of Birth: " + prophet.birthplace;
+            p2.textContent = "Place of Birth: " + prophet.birthplace
+            p3.textContent = "Place of Birth: " + prophet.birthplace;
             image.setAttribute('src', prophet.imageurl);
             image.setAttribute('alt', fullName + " - " + prophet.order);
 
