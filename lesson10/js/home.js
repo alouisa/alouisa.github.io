49d403d2a5aaadec1d7d5d1af2e0d1f10424dd35
alouisa <annaastle5@gmail.com>
2020-06-22 11:26:09 -0700

    commit

---
+++ lesson10/js/home.js
@@ -0,0 +1,43 @@
+const requestURL = 'https:////byui-cit230.github.io/weather/data/towndata.json';
+fetch(requestURL)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (jsonObject) {
+        // console.table(jsonObject);  // temporary checking for valid response and data parsing
+        const towns = jsonObject['towns'];
+        towns.forEach(town => {
+            if (town.name == "Fish Haven" || town.name == "Preston" || town.name == "Soda Springs"){
+                let card = document.createElement('section')
+                let div = document.createElement('div')
+                let h3 = document.createElement('h3');
+                let p1 = document.createElement('p');
+                let p2 = document.createElement('p');
+                let p3 = document.createElement('p');
+                let p4 = document.createElement('p');
+                let image = document.createElement('img');
+
+                div.setAttribute('class', 'townData')
+                h3.textContent = town.name;
+                h3.setAttribute('class', 'townName');
+                p1.textContent = town.motto;
+                p1.setAttribute('class', 'townMoto');
+                p2.textContent = "Year Founded: " + town.yearFounded;
+                p3.textContent = "Population: " + town.currentPopulation;
+                p4.textContent = "Annual Rain Fall: " + town.averageRainfall;
+                image.setAttribute('src', `images/home/${town.photo}`);
+                image.setAttribute('alt', town.name);
+
+                div.appendChild(h3);
+                div.appendChild(p1);
+                div.appendChild(p2);
+                div.appendChild(p3);
+                div.appendChild(p4);
+                card.appendChild(div);
+                card.appendChild(image);
+
+                document.querySelector('div.cards').appendChild(card);
+            }
+        });
+
+    });
