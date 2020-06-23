6280f9b7da86adb7d3bdd9cf81a588625a151b39
alouisa <annaastle5@gmail.com>
2020-06-22 20:20:30 -0700

    commit

--- lesson10/js/weatherForcast.js
+++ lesson10/js/weatherForcast.js
@@ -2,7 +2,7 @@ const requestURL = "http://api.openweathermap.org/data/2.5/forecast?id=5604473&A
 fetch(requestURL)
     .then((response) => response.json())
     .then((jsObject) => {
-        console.log(jsObject);
+        // console.log(jsObject);
         document.getElementById('current-temp').textContent = jsObject.main.temp;
         const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.weather[0].icon + '.png';  // note the concatenation
         const desc = jsObject.weather[0].description;  // note how we reference the weather array
