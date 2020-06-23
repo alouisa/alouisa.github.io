15f3af60f38384fd30457a89bfb967afbef19cbf
alouisa <annaastle5@gmail.com>
2020-06-23 10:24:38 -0700

    commit

--- lesson10/js/weatherForcast.js
+++ lesson10/js/weatherForcast.js
@@ -2,15 +2,14 @@ const requestURL = "http://api.openweathermap.org/data/2.5/forecast?id=5604473&A
 fetch(requestURL)
     .then((response) => response.json())
     .then((jsObject) => {
-        console.log(jsObject);
-        document.getElementById('current-temp').textContent = Math.round(jsObject.list[20].main.temp * (9 / 5) - 459.67);
-        const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.list[20].weather[0].icon + '.png';
-        const desc = jsObject.list[20].weather[0].description;
+        // console.log(jsObject);
+        document.getElementById('current-temp').textContent = jsObject.main.temp;
+        const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.weather[0].icon + '.png';  // note the concatenation
+        const desc = jsObject.weather[0].description;  // note how we reference the weather array
         document.getElementById('imagesrc').textContent = imagesrc;  // informational specification only
-        document.getElementById('icon').setAttribute('src', imagesrc);
+        document.getElementById('icon').setAttribute('src', imagesrc);  // focus on the setAttribute() method
         document.getElementById('icon').setAttribute('alt', desc);
-    });
-)
+    })
 
 
 let t = parseFloat(document.querySelector("#highTemp").innerHTML);
