93d3286c35164181884c7f2a58472f8e8702a63f
alouisa <annaastle5@gmail.com>
2020-06-25 12:20:15 -0700

    commit

--- lesson10/js/weatherapi.js
+++ lesson10/js/weatherapi.js
@@ -1,11 +1,11 @@
-const requestURL = "http://api.openweathermap.org/data/2.5/forecast?id=5604473&APPID=afbcf6ac456ba0edf76d17cd9722668b&units=imperial";
+const requestURL = "http://api.openweathermap.org/data/2.5/weather?id=5604473&APPID=afbcf6ac456ba0edf76d17cd9722668b&units=imperial";
 fetch(requestURL)
     .then((response) => response.json())
     .then((jsObject) => {
         console.log(jsObject);
-        document.getElementById('current-temp').textContent = Math.round(jsObject.list[20].main.temp);
-        const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.list[20].weather[0].icon + '.png'; 
-        const desc = jsObject.list[20].weather[0].description; 
+        document.getElementById('current-temp').textContent = Math.round(jsObject.main.temp);
+        const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.weather[0].icon + '.png'; 
+        const desc = jsObject.weather[0].description; 
         document.getElementById('imagesrc').textContent = imagesrc;  // informational specification only
         document.getElementById('icon').setAttribute('src', imagesrc); 
         document.getElementById('icon').setAttribute('alt', desc);
