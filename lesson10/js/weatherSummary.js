90fb39668cb784459b5ccc3eca61a467bfbdf38f
alouisa <annaastle5@gmail.com>
2020-06-25 12:49:59 -0700

    commit

---
+++ lesson10/js/weatherSummary.js
@@ -0,0 +1,25 @@
+const requestURL = "http://api.openweathermap.org/data/2.5/weather?id=5604473&APPID=afbcf6ac456ba0edf76d17cd9722668b&units=imperial";
+fetch(requestURL)
+    .then((response) => response.json())
+    .then((jsObject) => {
+        console.log(jsObject);
+        let t = jsObject.main.temp;
+        let s = jsObject.wind.speed;
+        document.getElementById('current-temp').textContent = t;
+        let windchill = 35.74 + 0.6215 * t - 35.75 * Math.pow(s, 0.16) + 0.4275 * t * Math.pow(s, 0.16);
+        if (t <= 50 && s >= 3.0) {
+            document.querySelector("#windchill").innerHTML = Math.round(windchill);
+        }
+        else {
+            document.querySelector("#windchill-container").innerHTML = "Wind Chill: N/A";
+        }
+
+        const imagesrc = `https://openweathermap.org/img/w/${jsObject.weather[0].icon}.png`; 
+        const desc = jsObject.weather[0].description;  
+        document.getElementById('icon').setAttribute('src', imagesrc);  
+        document.getElementById('icon').setAttribute('alt', desc);
+        
+    })
+
+
+
