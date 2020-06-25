5607d1fc9607ec09749c084b2ead71535b667059
alouisa <annaastle5@gmail.com>
2020-06-24 22:33:10 -0700

    commit

--- lesson10/js/weatherapi.js
+++ lesson10/js/weatherapi.js
@@ -1,9 +1,9 @@
-const requestURL = "http://api.openweathermap.org/data/2.5/forecast?id=5604473&APPID=afbcf6ac456ba0edf76d17cd9722668b";
+const requestURL = "http://api.openweathermap.org/data/2.5/forecast?id=5604473&APPID=afbcf6ac456ba0edf76d17cd9722668b&units=imperial";
 fetch(requestURL)
     .then((response) => response.json())
     .then((jsObject) => {
         console.log(jsObject);
-        document.getElementById('current-temp').textContent = Math.round(jsObject.list[20].main.temp * (9 / 5) - 459.67);
+        document.getElementById('current-temp').textContent = Math.round(jsObject.list[20].main.temp);
         const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.list[20].weather[0].icon + '.png'; 
         const desc = jsObject.list[20].weather[0].description; 
         document.getElementById('imagesrc').textContent = imagesrc;  // informational specification only
