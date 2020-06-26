e1d5f0569d658aef370b10b634dc406c82d4bb1d
alouisa <annaastle5@gmail.com>
2020-06-25 21:52:43 -0700

    commit

--- lesson10/js/weatherSummary.js
+++ lesson10/js/weatherSummary.js
@@ -1,14 +1,16 @@
 const requestURL = "http://api.openweathermap.org/data/2.5/weather?id=5604473&APPID=afbcf6ac456ba0edf76d17cd9722668b&units=imperial";
 fetch(requestURL)
     .then((response) => response.json())
-    .then((jsObject) => {;
+    .then((jsObject) => {
+        console.log(jsObject);
         let highT = jsObject.main.temp_max;
         let t = jsObject.main.temp;
         let s = jsObject.wind.speed;
         let windchill = 35.74 + 0.6215 * t - 35.75 * Math.pow(s, 0.16) + 0.4275 * t * Math.pow(s, 0.16);
+        document.getElementById('weatherDescr').textContent = jsObject.weather[0].description;
         document.getElementById('current-temp').textContent = Math.round(t);
         document.getElementById('highTemp').innerHTML = `${Math.round(highT)}&deg;F`;
-        document.getElementById('weatherDescr').textContent = jsObject.weather[0].description;
+        document.getElementById('windSpeed').textContent = Math.round(s);
         if (t <= 50 && s >= 3.0) {
             document.querySelector("#windchill").innerHTML = Math.round(windchill);
         }
