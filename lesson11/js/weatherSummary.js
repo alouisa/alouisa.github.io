0f6f71941c6981ca1bbf04edad21b10d88892f8f
alouisa <annaastle5@gmail.com>
2020-06-27 13:00:13 -0700

    commit

--- lesson11/js/weatherSummary.js
+++ lesson11/js/weatherSummary.js
@@ -7,11 +7,11 @@ fetch(requestURL)
         let s = jsObject.wind.speed;
         let windchill = 35.74 + 0.6215 * t - 35.75 * Math.pow(s, 0.16) + 0.4275 * t * Math.pow(s, 0.16);
         document.getElementById('weatherDescr').textContent = jsObject.weather[0].description;
-        document.getElementById('current-temp').textContent = Math.round(t);
-        document.getElementById('highTemp').innerHTML = `${Math.round(highT)}&deg;F`;
-        document.getElementById('windSpeed').textContent = Math.round(s);
+        document.getElementById('current-temp').textContent = Math.floor(t);
+        document.getElementById('highTemp').innerHTML = `${Math.floor(highT)}&deg;F`;
+        document.getElementById('windSpeed').textContent = Math.floor(s);
         if (t <= 50 && s >= 3.0) {
-            document.querySelector("#windchill").innerHTML = Math.round(windchill);
+            document.querySelector("#windchill").innerHTML = Math.floor(windchill);
         }
         else {
             document.querySelector("#windchill-container").innerHTML = "Wind Chill: N/A";
@@ -33,7 +33,7 @@ fetch(forecastURL)
     let d = new Date(day).getDay()
     const srcset = `https://openweathermap.org/img/w/${forecast.weather[0].icon}.png`;
     document.querySelector(`#day${i}`).textContent = weekdays[d];
-    document.querySelector(`#forecast${i}`).innerHTML = Math.round(forecast.main.temp) +"&deg;F"; 
+    document.querySelector(`#forecast${i}`).innerHTML = Math.floor(forecast.main.temp) +"&deg;F"; 
     document.querySelector(`#icon${i}`).setAttribute('src', srcset);
     document.querySelector(`#icon${i}`).setAttribute('alt', forecast.weather[0].description);
        i++; 
