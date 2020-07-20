09814e53606887bbbe577dcca7217c79acbfe395
alouisa <annaastle5@gmail.com>
2020-07-20 15:53:42 -0600

    commit

--- scoot/js/rentalprices.js
+++ scoot/js/rentalprices.js
@@ -3,13 +3,26 @@ fetch(requestURL)
     .then((response) => response.json())
     .then((jsObject) => {
         console.log(jsObject);
+        let rentalData = jsObject['rentalData'];
         let tbody = document.querySelector('#rentalPricesTable tbody');
-        jsObject.prices.forEach(element => {
-            let rentaltype = ;
-            let maxpersons = ;
-            let halfdayRes = ;
-            let fulldayRes = ;
-            let halfdayNoRes = ;
-            let fulldayNoRes = ;
+        rentalData.forEach(element => {
+            const newRow = document.createElement('tr');
+            const rentaltype = document.createElement('td');
+            const maxpersons = document.createElement('td');
+            const halfdayRes = document.createElement('td');
+            const fulldayRes = document.createElement('td');
+            const halfdayNoRes = document.createElement('td');
+            const fulldayNoRes = document.createElement('td');
+
+            newRow.appendChild(rentaltype);
+            newRow.appendChild(maxpersons);
+            newRow.appendChild(rentaltype);
+            newRow.appendChild(halfdayRes);
+            newRow.appendChild(fulldayRes);
+            newRow.appendChild(halfdayNoRes)
+            newRow.appendChild(fulldayNoRes)
+                              
+            tbody.appendChild(newRow);
+           ;
         });
     });
