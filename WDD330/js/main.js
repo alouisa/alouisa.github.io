9a7d48b84d242ab550d49d160199b2a93a052855
alouisa <annaastle5@gmail.com>
2021-01-02 21:18:35 -0800

    .

--- WDD330/js/main.js
+++ WDD330/js/main.js
@@ -10,7 +10,7 @@ let htmlList = document.getElementById("linkList");
 for(let i=0; i < links.length; i++){
     let li = document.createElement('li');
     let a = document.createElement('a'); 
-    a.setAttribute("href", `"${links[i]["url"]}"`);
+    a.setAttribute("href", links[i]["url"]);
     a.innerText = links[i]["label"];
     li.appendChild(a); 
     htmlList.appendChild(li);
