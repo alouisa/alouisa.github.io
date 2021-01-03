af623e98a3eb9dc3aebe068a190d734fb5f402a9
alouisa <annaastle5@gmail.com>
2021-01-02 20:10:16 -0800

    .

--- WDD330/js/main.js
+++ WDD330/js/main.js
@@ -1,7 +1,7 @@
 linkList = ["testLink1", "testLink2"]
 let htmlList = document.getElementById("linkList");
 
-for(let i=0; i < linkList.length -1; i++){
+for(let i=0; i < linkList.length; i++){
     let li = document.createElement('li');
     li.innerHTML = linkList[i]; 
     htmlList.appendChild(li);
