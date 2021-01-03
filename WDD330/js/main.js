2f794ce733b3fd41ea695e50fdbf48958eaf45ff
alouisa <annaastle5@gmail.com>
2021-01-02 20:03:34 -0800

    fixed length function

--- WDD330/js/main.js
+++ WDD330/js/main.js
@@ -1,7 +1,7 @@
 linkList = ["testLink1", "testLink2"]
 let htmlList = document.getElementById("linkList");
 
-for(let i=0; i < length(linkList)-1; i++){
+for(let i=0; i < linkList.length -1; i++){
     let li = Document.createElement('li');
     li.createTextNode(i); 
     htmlList.appendChild(li);
