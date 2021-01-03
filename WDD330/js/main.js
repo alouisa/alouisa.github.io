d9032f194f03e7e30d63102e80863ee1aef3b0c5
alouisa <annaastle5@gmail.com>
2021-01-02 21:11:38 -0800

    javascript list

--- WDD330/js/main.js
+++ WDD330/js/main.js
@@ -10,8 +10,8 @@ let htmlList = document.getElementById("linkList");
 for(let i=0; i < links.length; i++){
     let li = document.createElement('li');
     let a = document.createElement('a'); 
-    a.setAttribute("href", links[i]["url"])
-    a.textContent = links[i]["label"];
-    li.innerHTML = a; 
+    a.setAttribute("href", `"${links[i]["url"]}"`);
+    a.innerText = links[i]["label"];
+    li.appendChild(a); 
     htmlList.appendChild(li);
 }
