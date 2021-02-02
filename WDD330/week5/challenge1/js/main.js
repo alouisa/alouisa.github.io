c7bf9036c88d2c47afac41700714e68686591d52
alouisa <annaastle5@gmail.com>
2021-02-01 21:25:22 -0800

    c

--- WDD330/week5/challenge1/js/main.js
+++ WDD330/week5/challenge1/js/main.js
@@ -2,11 +2,13 @@ const todoItemInput = document.querySelector(".todoItemInput");
 const toDoList = document.querySelector(".toDoList"); 
 
 function addListItem(){
-    newItem = document.createElement("input");
-    newItemLabel = document.createElement("label");
-    newItem.setAttribute("type", "checkbox");
-    newItemLabel.inneHTML = "hello";
-    newItemLabel.appenChild(newItem);
+    checkbox = document.createElement("INPUT");
+    label = document.createElement("LABEL");
+    checkbox.setAttribute("type", "checkbox");
+    checkbox.setAttribute("id", "checkbox1");
+    label.innerHTML = "test";
+    label.setAttribute("for", "checkbox1");
 
-    toDoList.appendChild(newItemLabel);
+    toDoList.appendChild(checkbox);
+    toDoList.appendChild(label)
 }
