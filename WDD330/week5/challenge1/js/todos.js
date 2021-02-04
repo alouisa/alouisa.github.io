34803bbde1bb676fe05588d98bf67e09d30318fc
alouisa <annaastle5@gmail.com>
2021-02-04 07:45:56 -1000

    begun todo class

--- WDD330/week5/challenge1/js/todos.js
+++ WDD330/week5/challenge1/js/todos.js
@@ -1,14 +1,16 @@
 //import utilites.js/ helper functions
 //add variable to store list of tasks
-class Todos(){ 
-    this.todoList = null;
+class Todos { 
+    constructor(){
+        this.todoList = null;
+    }
 
     addToDo(){
         
     }
 
     saveToDo(task, key){
-
+        
     }
 
     getToDos(key){
