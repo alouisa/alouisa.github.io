31e551f6163a73e2168495ff3a659383ba994635
alouisa <annaastle5@gmail.com>
2021-03-11 11:15:50 -0800

    added auto filling categories

--- WDD330/week10/triviaApp/js/main.js
+++ WDD330/week10/triviaApp/js/main.js
@@ -1,3 +1,23 @@
-fetch('https://opentdb.com/api.php?amount=10')
-    .then(response => response.json())
-    .then(data => console.log(data))
+let categoryParent = document.getElementById('categories');
+
+function fetchCategory(){
+    fetch(`https://opentdb.com/api.php?amount=1&type=multiple`)
+        .then(response => response.json())
+        .then(data => setCategory(data.results[0]['category'], categoryParent));
+}
+
+function setCategories(){
+    for (let i = 0; i < 6; i++) {
+       fetchCategory(); 
+}
+}
+
+function setCategory(item, parent){
+    let li = document.createElement('li');
+    li.innerHTML = item;
+    parent.appendChild(li);
+}
+
+setCategories();
+
+console.log(categoryParent.children);
