411431a47ceb15508c575899ced23dd83d577997
alouisa <annaastle5@gmail.com>
2021-03-16 14:24:35 -0700

    changed categories to be recorded by number like the API

--- WDD330/week11/triviaApp/js/categories.js
+++ WDD330/week11/triviaApp/js/categories.js
@@ -1,28 +1,46 @@
 let categoryParent = document.getElementById('categories');
 let categoryNames = [];
+var categoryList = [];
 
-function fetchCategory() {
-    return fetch(`https://opentdb.com/api.php?amount=1&type=multiple`)
-        .then(response => response.json())
-        .then(data => data.results[0])
+function randomInt(min, max) {
+    return Math.ceil(Math.random() * (max - min) + min);
 }
-
-async function createCategoryList(list, listLength) {
+function createRandomList(listLength, min, max) {
+    let randomList = [];
     for (let i = 0; i < listLength;) {
-        let category = await fetchCategory();
-        if (!categoryNames.includes(category["category"])) {
-            categoryNames.push(category["category"]);
-            setCategory(category["category"], categoryParent);
+        let num = randomInt(min, max);
+        if (!randomList.includes(num)) {
+            randomList.push(num);
             i++;
         }
     }
+    return randomList;
 }
-function setCategory(item, parent){
+
+function fetchCategory(category, amount) {
+    return fetch(`https://opentdb.com/api.php?amount=${amount}&type=multiple&category=${category}`)
+        .then(response => response.json())
+        .then(data => data.results)
+        .catch(error => console.log(error));
+}
+
+async function createCategoryList(list1, list2) {
+    const categoryList = createRandomList(6, 9, 25);
+    categoryList.forEach(async item => {
+        let category = await fetchCategory(item, 10);
+        let categoryTitle = category[0]["category"];
+        list1.push(category);
+        list2.push(categoryTitle);
+        setCategory(categoryTitle, categoryParent);
+    });
+}
+
+function setCategory(item, parent) {
     let li = document.createElement('li');
-        li.innerText = item;
-        parent.appendChild(li);
+    li.innerText = item;
+    parent.appendChild(li);
 
 }
 
-createCategoryList(categoryNames, 6);
-console.log(categoryNames);
+createCategoryList(categoryList, categoryNames, 6);
+console.log(categoryList);
