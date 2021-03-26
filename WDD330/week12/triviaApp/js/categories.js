e66f00e2fd0490ff36d5fcb3a3e2005732e3f932
alouisa <annaastle5@gmail.com>
2021-03-26 08:52:41 -0700

    button choose new category refresh

--- WDD330/week12/triviaApp/js/categories.js
+++ WDD330/week12/triviaApp/js/categories.js
@@ -110,7 +110,7 @@ function quizSetup(category, parent, i = 0){
     }
     else{
         hide(nextBtn);
-        continueBtn.classList.remove('hide');
+        show(continueBtn);
     }
 }
 
@@ -159,3 +159,13 @@ function displayScore(){
 
 createCategoryList(categoryList, categoryNames, 6);
 addCategoryEvent(categoryNames, categoryList, categories, quizList, quizTitle);
+
+
+// continueBtn.addEventListener('click', ()=>{
+//     hide(quizParent); 
+//     hide(continueBtn);
+//     categories.innerHTML = '';
+//     createCategoryList(categoryList, categoryNames, 6);
+//     show(categories.parentElement.parentElement);
+//     addCategoryEvent(categoryNames, categoryList, categories, quizList, quizTitle);
+// });
