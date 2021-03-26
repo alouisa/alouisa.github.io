7f901eb264e7de61abb25089a2d39447515ec1f1
alouisa <annaastle5@gmail.com>
2021-03-26 08:59:40 -0700

    added week13

---
+++ WDD330/week13/triviaApp/js/main.js
@@ -0,0 +1,4 @@
+import { createCategoryList, addCategoryEvent } from './categories.js';
+
+createCategoryList(categoryList, categoryNames, 6);
+addCategoryEvent(categoryNames, categoryList, categories, quizList, quizTitle);
